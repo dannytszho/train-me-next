4518048affa03d2f0be96b595f8c3b5d1b45d222
dannytszho <dannytszho@gmail.com>
2022-04-13 02:30:59 +0800

    fix types in Stopwatch

--- pages/stopwatch.tsx
+++ pages/stopwatch.tsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
-const formatNumber = (number) => `0${number}`.slice(-2);
+interface IProps {
+  number: number;
+  time: number;
+  mins: number;
+  sec: number;
+  ms: number;
+}
 
-const getRemaining = (time) => {
-  const mins = Math.floor(time / 60);
-  const secs = time - mins * 60;
-  return { mins: formatNumber(mins), secs: formatNumber(secs) };
+const formatNumber = (number: IProps) => `0${number}`.slice(-2);
+
+const getRemaining = (time: IProps) => {
+  const mins = Math.floor((+time / 60000) % 60);
+  const secs = Math.floor((+time / 1000) % 60);
+  const ms = (+time / 10) % 1000;
+  return {
+    mins: formatNumber(mins as any),
+    secs: formatNumber(secs as any),
+    ms: formatNumber(ms as any),
+  };
 };
 const StopWatch = () => {
   const [remainingSecs, setRemainingSecs] = useState(0);
   const [isActive, setIsActive] = useState(false);
-  const { mins, secs } = getRemaining(remainingSecs);
+  const { mins, secs, ms } = getRemaining(remainingSecs as any);
 
   const toggle = () => {
     setIsActive(!isActive);
@@ -25,8 +38,8 @@ const StopWatch = () => {
     let interval: any = null;
     if (isActive) {
       interval = setInterval(() => {
-        setRemainingSecs((remainingSecs) => remainingSecs + 1);
-      }, 1000);
+        setRemainingSecs((remainingSecs) => remainingSecs + 10);
+      }, 10);
     } else if (!isActive && remainingSecs !== 0) {
       clearInterval(interval);
     }
@@ -35,7 +48,13 @@ const StopWatch = () => {
 
   return (
     <div>
-      <h3>{`${mins}:${secs}`}</h3>
+      <h3>{`${mins}:${secs}:${ms}`}</h3>
+      <button onClick={toggle} type="button">
+        {isActive ? 'Pause' : 'Start'}
+      </button>
+      <button onClick={reset} type="button">
+        Reset
+      </button>
     </div>
   );
 };
